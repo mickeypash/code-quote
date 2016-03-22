56c433dd23b0e84033c91adae83f4adb121c4b5d
Mickey Pashov <pashov.m@gmail.com>
2016-03-22 23:16:23 +0000

    Added basic models Author, Quote, Topic

--- quote/models.py
+++ quote/models.py
@@ -2,4 +2,21 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+
 # Create your models here.
+class Author(models.Model):
+    first_name = models.CharField(max_length=60)
+    last_name = models.CharField(max_length=60)
+    dob = models.DateTimeField('date published')
+
+
+class Quote(models.Model):
+    quote_text = models.CharField(max_length=200)
+    authored_date = models.DateTimeField('date published')
+    pub_date = models.DateTimeField('date published')
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+
+
+class Topic(models.Model):
+    name = models.CharField(max_length=60)
